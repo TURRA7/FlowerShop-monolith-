8d370c281ee62a0c2ed88cea10ecdec261e2a4ed
TURRA7 <turra777@mail.ru>
2024-04-01 13:20:18 +0300

    master
    Был переделан файл config.py, теперь настройки(под конкретные ситуации), распределены по классам (ООП), для более удобной и гибкой настройки. Так же было изменено подключение к конфигурационному файлу в handler_flask.py

--- handlers/config.py
+++ handlers/config.py
@@ -1,16 +1,46 @@
-"""Модуль представляет собой конфигурационный файл."""
+"""Модуль представляет собой вариации конфигурации."""
 from datetime import timedelta
 
-SECRET_KEY = "5f98079c7458d648234ee38b2d3dbd0ed78817f09f5cf225379dd0d5c8d78f85"
-CSRF_ENABLED = True
-DEBUG = True
-SESSION_COOKIE_NAME = "session"
-PERMANENT_SESSION_LIFETIME = timedelta(days=31)
-SESSION_REFRESH_EACH_REQUEST = True
-EXPLAIN_TEMPLATE_LOADING = True
-MAX_COOKIE_SIZE = 0
-ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-UPLOAD_FOLDER = 'handlers/static/img/uploads'
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Vbn_78900@localhost:5432"
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class Config:
+    """Общие настройки для разработки и продакшена."""
+
+    MAX_COOKIE_SIZE = 0
+    ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+    UPLOAD_FOLDER = 'handlers/static/img/uploads'
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = "5f98079c7458d648234ee38b2d3dbd0ed78817f0"
+    CACHE_TYPE = "SimpleCache"
+    CACHE_DEFAULT_TIMEOUT = 2592000
+    SESSION_COOKIE_NAME = "session"
+    CSRF_ENABLED = True
+    PERMANENT_SESSION_LIFETIME = timedelta(days=31)
+    SESSION_REFRESH_EACH_REQUEST = True
+    EXPLAIN_TEMPLATE_LOADING = True
+
+
+class DevelopmentConfig(Config):
+    """Настройки для разработки."""
+
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Vbn_78900@localhost:5432"
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    """Настройки для продакшена."""
+
+    CACHE_TYPE = "..."
+    SECRET_KEY = "..."
+    SESSION_COOKIE_NAME = "..."
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:login@host:port"
+    DEBUG = False
+
+
+class TestingConfig(Config):
+    """Настройки для тестирования."""
+
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Vbn_78900@localhost:5432"
+    DEBUG = True
+    TESTING = True
